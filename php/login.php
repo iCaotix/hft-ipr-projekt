48dc6b0a237c7dd32e30afc00b82df9b5f3b6cca
wiecktobi <39270895+wiecktobi@users.noreply.github.com>
2018-06-08 17:29:14 +0200

    Add files via upload

--- php/login.php
+++ php/login.php
@@ -1,19 +1,22 @@
 <?php
 
-require('dbConnect.php');
-if (isset($_POST['index.php'])) {
+  require('../dbConnect.php');
 
-  $name = $_POST['loginName'];
-  $pswd = $_POST['loginPassword'];
+  $name = $database->real_escape_string($_POST['registerName']);
+  $pswd = $database->real_escape_string($_POST['registerPassword']);
+  $email = $database->real_escape_string($_POST['registerMail']);
 
-  $abfrage = "SELECT PASSWORD WHERE loginName=NAME";
+
+  $abfrage = "SELECT password WHERE $name=user";
   if ($pswd == $abfrage) {
     // code..
+    echo "HI";
   }
-}
-
-
-
-
+  // if ($abfrage == "") {
+    $stmt = $database->prepare("INSERT INTO user(user, email, password) VALUES (?,?,?)");
+    $stmt->bind_param("sss", $name, $email, $pswd);
+    $stmt->execute();
+    $stmt->close();
+  // }
 
  ?>
