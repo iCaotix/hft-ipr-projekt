359e30d7a465c9e6b7489e097c5988c42602eb4f
Marcel Schwarz <marcel@icaotix.de>
2018-06-17 18:24:56 +0200

    made login synchronous
    Wegen des async aufrufs des login php scripts konnte die session nicht richtig gesetzt werden, deshalb ist der login jetzt syncron. Die Registrierung ist weiterhin asyncron.

--- js/contentloader.js
+++ js/contentloader.js
@@ -21,7 +21,7 @@ async function getNewUniqueLink() {
 document.getElementById('btnCreate').addEventListener("click" , async (event) => {
   const formData = new FormData(document.getElementById('create-form'));
   try {
-    const response = await fetch('php/signin.php', {
+    const response = await fetch('php/signup.php', {
       method: 'post',
       body: formData
     });
@@ -31,45 +31,4 @@ document.getElementById('btnCreate').addEventListener("click" , async (event) =>
   } catch (e) {
     console.log("Abschicken der Daten fehlerhaft!");
   }
-})
-
-//<!--Login-->
-document.getElementById('btnLogin').addEventListener("click" , async (event) => {
-  event.preventDefault();
-  // if (document.getElementById('btnLogin') === "Login") {
-    const formData = new FormData(document.getElementById('login-form'));
-
-    // try {
-      const response = await fetch('php/signup.php', {
-        method: 'post',
-        body: formData
-      });
-      const jsonData = await response.json();
-      if (typeof jsonData.error !== 'undefined') {
-        console.log(jsonData);
-        document.getElementById('login-status').innerHTML = jsonData.error.fontcolor("red");
-      } else {
-        console.log(jsonData);
-        document.getElementById('login-status').innerHTML = "Eingeloggt";
-        location.replace('index2.htm');
-        // if (jsonData == 'true') {
-        //   document.getElementById('toggle-login').innerHTML = "Logout";
-        //   document.getElementById('btnLogin').innerHTML = "Logout";
-        //   document.getElementById('exampleModalLabel').innerHTML = "Logout";
-        //   document.getElementById('login-status').innerHTML = "Erfolgreich eingeloggt";
-        // }
-        // else {
-        //   document.getElementById('toggle-login').innerHTML = "Login";
-        //   document.getElementById('btnLogin').innerHTML = "Login";
-        //   document.getElementById('exampleModalLabel').innerHTML = "Login";
-        //   document.getElementById('login-status').innerHTML = "Erfolgreich ausgeloggt";
-        // }
-        // location.reload();
-      }
-    // }
-    // catch (e) {
-    //   console.log("Login Fehler");
-    // }
-  // }
-
 })
--- php/signup.php
+++ php/signup.php
@@ -1,33 +1,26 @@
 <?php
-  //<!--Login-->
-  session_start();
+  //<!--Register-->
   require('../dbConnect.php');
 
+  $name = ($_POST['registerName']);
+  $pswd = ($_POST['registerPassword']);
+  $email = ($_POST['registerMail']);
 
-  $name = ($_POST['loginName']);
-  $pswd = ($_POST['loginPassword']);
-  // $pswd = md5($pswd);
+  $search_user = $database->prepare("SELECT ID FROM user WHERE user='$name '");
+  $search_user->execute();
+  $search_result = $search_user->get_result();
 
-  $stmt = $database->prepare("SELECT id FROM user WHERE '$name'=user AND password='$pswd'");
-  $stmt->execute();
-  $result = $stmt->get_result();
-
-  if ($result->num_rows == 1) {
-    $object = $result->fetch_object();
-    $_SESSION['user'] = $object->id;
-    // header('Location: /index2.htm');
-    echo json_encode('Eingeloggt!');
-    // $stmt = $database->prepare("SELECT loggedin FROM user WHERE '$name'=user");
-    // $stmt->execute();
-    // $result = $stmt->get_result()->fetch_assoc();
-    // if ($result['loggedin'] == 'false') {
-    //   $stmt = $database->prepare("UPDATE `benutzer`.`user` SET `loggedin`='true' WHERE  '$name'=user");
-    //   $stmt->execute();
-    //   $a = "true";
-    } else {
-      $a = array('error' => 'Einlogen fehlgeschlagen!');
-      echo json_encode($a);
-    }
+  if ($search_result->num_rows == 0) {
+    $pswd = md5($pswd);
+    $stmt = $database->prepare("INSERT INTO user(user, email, password) VALUES (?,?,?)");
+    $stmt->bind_param("sss", $name, $email, $pswd);
+    $stmt->execute();
+    $stmt->close();
+    echo json_encode("Dein Account wurde erfolgreich erstellt!");
+  } else {
+    echo json_encode("Der Benutzername ist leider schon vergeben!");
+  }
 
 
-?>
+
+ ?>
