4fe2528aa77a4f0316c22219678963fcbc2ae44c
the_kellner <the_kellner@web.de>
2018-06-18 17:51:14 +0200

    ADD: example entry for myfrindsbook ->(segments/_myFreindsBook.php)

--- segments/_myFriendsBook.php
+++ segments/_myFriendsBook.php
@@ -1,2 +1,213 @@
-<h1>MYFRIENDSBOOK SPACER</h1>
-<button id="btnCreateTan" type="button" class="btn btn-info">Erstelle Tan</button>
+<p></p>
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <div class="alert alert-dark" role="alert">
+        <h4>Take a look in your FriendsBook!</h4>
+        <hr />
+        <button id="btnCreateTan" type="button" style="float: right;"class="btn btn-info">Erstelle Tan</button>
+        <h5>Want more entrys? go and invite your friends with the friendsTAN</h5>
+      </div>
+
+      <div class="card-columns">
+
+        <!--start of card -->
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">NAME, NACHNAME</h5>
+            <p class="card-text">Geburtstag: DB-data</p>
+            <p class="card-text">Wohnort: DB-data</p>
+            <p class="card-text">Straße: DB-data</p>
+            <p class="card-text">Woher kennen wir uns? DB-data laskdjflkjadsflk laskdjflaksdjfö laksjd flaksdjf ölkajd sflkja dsflkajs df</p>
+            <p class="card-text">Festnetz: DB-data</p>
+            <p class="card-text">Handynummer: DB-data</p>
+            <p class="card-text">E-Mail: DB-data</p>
+            <p class="card-text">Hobbies: DB-data</p>
+            <p class="card-text">Berufswunsch: DB-data</p>
+            <p class="card-text">Das könnte ich jeden Tag essen: DB-data</p>
+            <p class="card-text">Was ich auf eine Insel mitnehmen würde: DB-data</p>
+            <p class="card-text">Lieblingsfilm: DB-data</p>
+            <p class="card-text">Lieblingssport: DB-data</p>
+            <p class="card-text">Coolster Film oder Spielecharakter: DB-data</p>
+            <p class="card-text">Mein Lieblingstier: DB-data</p>
+            <p class="card-text">Lieblingsmusik: DB-data</p>
+            <p class="card-text">Geilstes Game: DB-data</p>
+            <p class="card-text">Lieblings alkoholisches Getränk: DB-data</p>
+            <p class="card-text">Meine heftigeste Suffstory: DB-data</p>
+            <p class="card-text">Letzter Absturz: DB-data</p>
+            <p class="card-text">Lieblings Trinkspiel: DB-data</p>
+          </div>
+        </div>
+        <!-- end of card -->
+
+        <!--start of card -->
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">NAME, NACHNAME</h5>
+            <p class="card-text">Geburtstag: DB-data</p>
+            <p class="card-text">Wohnort: DB-data</p>
+            <p class="card-text">Straße: DB-data</p>
+            <p class="card-text">Woher kennen wir uns? DB-data laskdjflkjadsflk laskdjflaksdjfö laksjd flaksdjf ölkajd sflkja dsflkajs df</p>
+            <p class="card-text">Festnetz: DB-data</p>
+            <p class="card-text">Handynummer: DB-data</p>
+            <p class="card-text">E-Mail: DB-data</p>
+            <p class="card-text">Hobbies: DB-data</p>
+            <p class="card-text">Berufswunsch: DB-data</p>
+            <p class="card-text">Das könnte ich jeden Tag essen: DB-data</p>
+            <p class="card-text">Was ich auf eine Insel mitnehmen würde: DB-data</p>
+            <p class="card-text">Lieblingsfilm: DB-data</p>
+            <p class="card-text">Lieblingssport: DB-data</p>
+            <p class="card-text">Coolster Film oder Spielecharakter: DB-data</p>
+            <p class="card-text">Mein Lieblingstier: DB-data</p>
+            <p class="card-text">Lieblingsmusik: DB-data</p>
+            <p class="card-text">Geilstes Game: DB-data</p>
+            <p class="card-text">Lieblings alkoholisches Getränk: DB-data</p>
+            <p class="card-text">Meine heftigeste Suffstory: DB-data</p>
+            <p class="card-text">Letzter Absturz: DB-data</p>
+            <p class="card-text">Lieblings Trinkspiel: DB-data</p>
+          </div>
+        </div>
+        <!-- end of card -->
+
+        <!--start of card -->
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">NAME, NACHNAME</h5>
+            <p class="card-text">Geburtstag: DB-data</p>
+            <p class="card-text">Wohnort: DB-data</p>
+            <p class="card-text">Straße: DB-data</p>
+            <p class="card-text">Woher kennen wir uns? DB-data laskdjflkjadsflk laskdjflaksdjfö laksjd flaksdjf ölkajd sflkja dsflkajs df</p>
+            <p class="card-text">Festnetz: DB-data</p>
+            <p class="card-text">Handynummer: DB-data</p>
+            <p class="card-text">E-Mail: DB-data</p>
+            <p class="card-text">Hobbies: DB-data</p>
+            <p class="card-text">Berufswunsch: DB-data</p>
+            <p class="card-text">Das könnte ich jeden Tag essen: DB-data</p>
+            <p class="card-text">Was ich auf eine Insel mitnehmen würde: DB-data</p>
+            <p class="card-text">Lieblingsfilm: DB-data</p>
+            <p class="card-text">Lieblingssport: DB-data</p>
+            <p class="card-text">Coolster Film oder Spielecharakter: DB-data</p>
+            <p class="card-text">Mein Lieblingstier: DB-data</p>
+            <p class="card-text">Lieblingsmusik: DB-data</p>
+            <p class="card-text">Geilstes Game: DB-data</p>
+            <p class="card-text">Lieblings alkoholisches Getränk: DB-data</p>
+            <p class="card-text">Meine heftigeste Suffstory: DB-data</p>
+            <p class="card-text">Letzter Absturz: DB-data</p>
+            <p class="card-text">Lieblings Trinkspiel: DB-data</p>
+          </div>
+        </div>
+        <!-- end of card -->
+
+        <!--start of card -->
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">NAME, NACHNAME</h5>
+            <p class="card-text">Geburtstag: DB-data</p>
+            <p class="card-text">Wohnort: DB-data</p>
+            <p class="card-text">Straße: DB-data</p>
+            <p class="card-text">Woher kennen wir uns? DB-data laskdjflkjadsflk laskdjflaksdjfö laksjd flaksdjf ölkajd sflkja dsflkajs df</p>
+            <p class="card-text">Festnetz: DB-data</p>
+            <p class="card-text">Handynummer: DB-data</p>
+            <p class="card-text">E-Mail: DB-data</p>
+            <p class="card-text">Hobbies: DB-data</p>
+            <p class="card-text">Berufswunsch: DB-data</p>
+            <p class="card-text">Das könnte ich jeden Tag essen: DB-data</p>
+            <p class="card-text">Was ich auf eine Insel mitnehmen würde: DB-data</p>
+            <p class="card-text">Lieblingsfilm: DB-data</p>
+            <p class="card-text">Lieblingssport: DB-data</p>
+            <p class="card-text">Coolster Film oder Spielecharakter: DB-data</p>
+            <p class="card-text">Mein Lieblingstier: DB-data</p>
+            <p class="card-text">Lieblingsmusik: DB-data</p>
+            <p class="card-text">Geilstes Game: DB-data</p>
+            <p class="card-text">Lieblings alkoholisches Getränk: DB-data</p>
+            <p class="card-text">Meine heftigeste Suffstory: DB-data</p>
+            <p class="card-text">Letzter Absturz: DB-data</p>
+            <p class="card-text">Lieblings Trinkspiel: DB-data</p>
+          </div>
+        </div>
+        <!-- end of card -->
+
+        <!--start of card -->
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">NAME, NACHNAME</h5>
+            <p class="card-text">Geburtstag: DB-data</p>
+            <p class="card-text">Wohnort: DB-data</p>
+            <p class="card-text">Straße: DB-data</p>
+            <p class="card-text">Woher kennen wir uns? DB-data laskdjflkjadsflk laskdjflaksdjfö laksjd flaksdjf ölkajd sflkja dsflkajs df</p>
+            <p class="card-text">Festnetz: DB-data</p>
+            <p class="card-text">Handynummer: DB-data</p>
+            <p class="card-text">E-Mail: DB-data</p>
+            <p class="card-text">Hobbies: DB-data</p>
+            <p class="card-text">Berufswunsch: DB-data</p>
+            <p class="card-text">Das könnte ich jeden Tag essen: DB-data</p>
+            <p class="card-text">Was ich auf eine Insel mitnehmen würde: DB-data</p>
+            <p class="card-text">Lieblingsfilm: DB-data</p>
+            <p class="card-text">Lieblingssport: DB-data</p>
+            <p class="card-text">Coolster Film oder Spielecharakter: DB-data</p>
+            <p class="card-text">Mein Lieblingstier: DB-data</p>
+            <p class="card-text">Lieblingsmusik: DB-data</p>
+            <p class="card-text">Geilstes Game: DB-data</p>
+            <p class="card-text">Lieblings alkoholisches Getränk: DB-data</p>
+            <p class="card-text">Meine heftigeste Suffstory: DB-data</p>
+            <p class="card-text">Letzter Absturz: DB-data</p>
+            <p class="card-text">Lieblings Trinkspiel: DB-data</p>
+          </div>
+        </div>
+        <!-- end of card -->
+
+        <!--start of card -->
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">NAME, NACHNAME</h5>
+            <p class="card-text">Geburtstag: DB-data</p>
+            <p class="card-text">Wohnort: DB-data</p>
+            <p class="card-text">Straße: DB-data</p>
+            <p class="card-text">Woher kennen wir uns? DB-data laskdjflkjadsflk laskdjflaksdjfö laksjd flaksdjf ölkajd sflkja dsflkajs df</p>
+            <p class="card-text">Festnetz: DB-data</p>
+            <p class="card-text">Handynummer: DB-data</p>
+            <p class="card-text">E-Mail: DB-data</p>
+            <p class="card-text">Hobbies: DB-data</p>
+            <p class="card-text">Berufswunsch: DB-data</p>
+            <p class="card-text">Das könnte ich jeden Tag essen: DB-data</p>
+            <p class="card-text">Was ich auf eine Insel mitnehmen würde: DB-data</p>
+            <p class="card-text">Lieblingsfilm: DB-data</p>
+            <p class="card-text">Lieblingssport: DB-data</p>
+            <p class="card-text">Coolster Film oder Spielecharakter: DB-data</p>
+            <p class="card-text">Mein Lieblingstier: DB-data</p>
+            <p class="card-text">Lieblingsmusik: DB-data</p>
+            <p class="card-text">Geilstes Game: DB-data</p>
+            <p class="card-text">Lieblings alkoholisches Getränk: DB-data</p>
+            <p class="card-text">Meine heftigeste Suffstory: DB-data</p>
+            <p class="card-text">Letzter Absturz: DB-data</p>
+            <p class="card-text">Lieblings Trinkspiel: DB-data</p>
+          </div>
+        </div>
+        <!-- end of card -->
+      </div>
+      <!-- end of card-colums-->
+
+
+
+
+<!-- jumbotron for a single entry -->
+<div class="jumbotron">
+  <h1 class="display-4">Name Nachname!</h1>
+  <p class="lead">Geburtstag:</p>
+  <p>DB-entry</p>
+  <hr/>
+  <p class="lead">Wohnort:</p>
+  <p>DB-entry</p>
+  <hr/>
+  <p class="lead">Straße:</p>
+  <p>DB-entry</p>
+  <hr/>
+  <p class="lead">Woher kennen wir uns:</p>
+  <p>DB-entry</p>
+  <hr/>
+</div>
+<!-- end of jumbotron -->
+
+</div>
+</div>
+</div> <!-- Ende container My FriendsBook -->
