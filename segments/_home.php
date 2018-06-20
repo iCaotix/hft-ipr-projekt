b1cb5523df70aa61ea6b60a366fe54ca7492ed84
Marcel Schwarz <marcel@icaotix.de>
2018-06-20 16:21:32 +0200

    added more home content

--- segments/_home.php
+++ segments/_home.php
@@ -1,3 +1,90 @@
+<p></p>
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <div class="card-columns">
+        <div class="card bg-success text-white">
+          <div class="card-body">
+            <h5 class="card-title">Hello and welcome on FriendsBook</h5>
+            <p class="card-text">You can write entys for your friends on the New Entry Page or create your own FriendsBook account to get your own entrys written by your frieds.</p>
+          </div>
+        </div>
+        <div class="card p-3">
+          <blockquote class="blockquote mb-0 card-body">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+            <footer class="blockquote-footer">
+              <small class="text-muted">
+                <a style="color: grey;" href="www.loremipsum.de">www.loremipsum.de</a>
+              </small>
+            </footer>
+          </blockquote>
+        </div>
+        <div class="card bg-warning text-white">
+          <div class="card-body">
+            <h5 class="card-title">Patch notes</h5>
+            <p class="card-text">In this patch we implemented a nicer coloring of the pages, also some important bug fixes are done.</p>
+            <p class="card-text">Please report all bugs and problems to our support mail adress: example-mail@support-friendsbook.com</p>
+            <p class="card-text"><small class="text-muted">Last updated 20.06.2018</small></p>
+          </div>
+        </div>
+        <div class="card bg-dark text-white text-center p-3">
+          <blockquote class="blockquote mb-0">
+            <p>"I choose a lazy person to do a hard job. Because a lazy person will find a easy way to do it."</p>
+            <footer class="blockquote-footer">
+              <small>
+                Bill Gates
+              </small>
+            </footer>
+          </blockquote>
+        </div>
+        <div class="card text-center bg-primary text-white">
+          <div class="card-body">
+            <h5 class="card-title">How to create a friendsTAN?</h5>
+            <p class="card-text">To create your friendsTAN just logg into your account and go to the My FriendsBook page, there you will find a create TAN button on the top of the page, you can also see your unused TANs there.</p>
+            <p class="card-text"><small class="text-white">FriendsBook</small></p>
+          </div>
+        </div>
+        <div class="card bg-dark text-white text-center p-3">
+          <blockquote class="blockquote mb-0">
+            <p class="card-text">"Everybodey should learn to programm a computer... because it teaches you how to think."</p>
+            <footer class="blockquote-footer">
+              <small>
+                Steve Jobs
+              </small>
+          </blockquote>
+        </div>
+        <div class="card p-3 bg-info text-white text-right">
+          <blockquote class="blockquote mb-0">
+            <p>We are online since 21.06.2018</p>
+            <footer class="blockquote-footer">
+              <small class="text-muted">
+                FriendsBook
+              </small>
+            </footer>
+          </blockquote>
+        </div>
+        <div class="card p-3 bg-secondary text-white">
+          <blockquote class="blockquote mb-0 card-body">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+            <footer class="blockquote-footer">
+              <small class="text-muted">
+                <a style="color: white;" href="www.loremipsum.de">www.loremipsum.de</a>
+              </small>
+            </footer>
+          </blockquote>
+        </div>
+        <div class="card bg-primary text-white text-center">
+          <div class="card-body">
+            <h5 class="card-title">How to write a new enntry?</h5>
+            <p class="card-text">To write a new enty for your friend you can go to the New Entry page, there you have to fill in all informations wich are marked. In the last field fill in the friendsTAN, please check twice that its correct, otherwise the entry is lost.</p>
+            <p class="card-text"><small class="text-white">FriendsBook</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> <!-- Ende container My FriendsBook -->
+
 <!-- PRICING -->
 <div class="alert pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center text-light">
   <h1 class="display-4">Pricing</h1>
